16ea9485c0aee0b6cdb20c2b190eee35586b008d
101 <tmddnjs001133@dsm.hs.kr>
2024-03-13 11:09:30 +0900

    update BowlingGameTest.java
    TDD :: RED
    add gutterGame test

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,6 +1,8 @@
 import bowlinggame.BowlingGame;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BowlingGameTest {
 
     private BowlingGame createNewBowlingGame() {
@@ -12,6 +14,17 @@ public class BowlingGameTest {
         BowlingGame bowlingGame = createNewBowlingGame();
 
         bowlingGame.roll(1);
+    }
+
+    @Test
+    public void gutterGame() {
+        BowlingGame bowlingGame = createNewBowlingGame();
+
+        for (int i = 0; i < 20; i++) {
+            bowlingGame.roll(0);
+        }
+
+        assertThat(bowlingGame.score(), 0);
     }
 
 }
