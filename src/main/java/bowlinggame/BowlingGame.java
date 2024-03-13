fec7a3a7b56eac6b1da38bf19b5d2de1bcbb2807
101 <tmddnjs001133@dsm.hs.kr>
2024-03-13 11:11:58 +0900

    update BowlingGame.java
    TDD :: GREEN
    add score method

--- src/main/java/bowlinggame/BowlingGame.java
+++ src/main/java/bowlinggame/BowlingGame.java
@@ -1,9 +1,14 @@
 package bowlinggame;
 
 public class BowlingGame {
+    private int score = 0;
 
     public void roll(int pins) {
         /* TODO document why this method is empty */
+    }
+
+    public int score() {
+        return score;
     }
 
 }
