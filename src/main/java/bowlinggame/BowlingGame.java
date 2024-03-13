799d13acc2f32d7e2e505c0b58c754cdb39a5b19
101 <tmddnjs001133@dsm.hs.kr>
2024-03-13 11:34:42 +0900

    refactor BowlingGame.java
    TDD :: BLUE

--- src/main/java/bowlinggame/BowlingGame.java
+++ src/main/java/bowlinggame/BowlingGame.java
@@ -1,14 +1,21 @@
 package bowlinggame;
 
 public class BowlingGame {
-    private int score = 0;
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
-        return score;
+        int totalScore = 0;
+
+        for (int i = 0; i < rolls.length; i++) {
+            totalScore += rolls[i];
+        }
+
+        return totalScore;
     }
 
 }
