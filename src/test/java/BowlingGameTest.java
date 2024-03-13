9bda485096bf09b8c461c5e2bc2dd39a26c37c2b
101 <tmddnjs001133@dsm.hs.kr>
2024-03-13 11:18:55 +0900

    update BowlingGameTest.java
    TDD :: RED
    add is score 18 test

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -26,6 +26,18 @@ public class BowlingGameTest {
         Assertions.assertEquals(bowlingGame.score(), 0);
     }
 
+    @Test
+    public void isScore18() {
+        BowlingGame bowlingGame = createNewBowlingGame();
+
+        bowlingGame.roll(7);
+        bowlingGame.roll(2);
+        bowlingGame.roll(8);
+        bowlingGame.roll(1);
+
+        Assertions.assertEquals(bowlingGame.score(), 18);
+    }
+
     private void setRolls(BowlingGame bowlingGame, int rolls, int pins) {
         for (int i = 0; i < rolls; i++) {
             bowlingGame.roll(pins);
