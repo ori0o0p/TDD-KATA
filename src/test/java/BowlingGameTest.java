006af7d4cb62100e2c4e884bea3151fb7b0d8567
101 <tmddnjs001133@dsm.hs.kr>
2024-03-13 11:03:21 +0900

    refactor BowlingGameTest.java
    TDD :: BLUE

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -3,9 +3,13 @@ import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
 
+    private BowlingGame createNewBowlingGame() {
+        return new BowlingGame();
+    }
+
     @Test
-    public void testBowlingGameCreation() {
-        BowlingGame bowlingGame = new BowlingGame();
+    public void canRoll() {
+        BowlingGame bowlingGame = createNewBowlingGame();
 
         bowlingGame.roll(1);
     }
